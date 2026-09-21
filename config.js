b96fa556867a3d49ea45e2770543d7a88ecc1702
otaviocamposotaviolaura-cyber <otaviocamposotaviolaura@gmail.com>
2026-09-21 09:01:48 -0300

    d

---
+++ config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "COLE_AQUI_A_PROJECT_URL";
+const SUPABASE_KEY = "COLE_AQUI_A_CHAVE_PUBLICA";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
